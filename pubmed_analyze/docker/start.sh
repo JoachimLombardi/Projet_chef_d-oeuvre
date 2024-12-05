5a5a70947b335e37b1cc4182f26beb0197ea83cb
Joachim Lombardi <lombardi.joachim@gmail.com>
2024-12-05 10:58:26 +0100

    test_docker

--- pubmed_analyze/docker/start.sh
+++ pubmed_analyze/docker/start.sh
@@ -1,7 +1,5 @@
-COMPOSE_FILE="pubmed_analyze/docker/docker-compose.yml"
-
 if [ -f .env ]; then
-  docker-compose -f $COMPOSE_FILE --env-file .env up --build -d
+  docker-compose --env-file .env up --build -d
 else
-  docker-compose -f $COMPOSE_FILE up --build -d
+  docker-compose up --build -d
 fi
